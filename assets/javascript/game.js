7316ee0fa241b0b040585d5018c1f8e577dbf483
nicholettesams <nicholettesams@msn.com>
2018-08-19 10:15:09 -0400

    Instead of object, now using attributes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,13 @@
-//Objects
-//Probably would be better to have a class called Pokemon and then create new objects of the Pokemon class
-var Venusar = {
-    name:"Vemusar", 
-    defaultHP: 100, 
-    currentHP: 100,
-    defaultAttackPower:9,
-    currentAttackPower: 9, //Keeps track of increasing attackPower after each attack
-    counterAttackPower: 50,
-    resetAttackPower: function() {this.currentAttackPower = this.defaultAttackPower;},
-    resetHP: function() {this.currentHP = this.defaultHP;}
-};
-
-var Blastoise = {
-    name:"Blastoise", 
-    defaultHP: 100, 
-    currentHP: 100,
-    defaultAttackPower:11,
-    currentAttackPower:11,
-    counterAttackPower: 50,
-    resetAttackPower: function() {this.currentAttackPower = this.defaultAttackPower;},
-    resetHP: function() {this.currentHP = this.defaultHP;}
-};
-
-var Charizard = {
-    name:"Charizard", 
-    defaultHP: 100, 
-    currentHP: 100, 
-    defaultAttackPower:7,
-    currentAttackPower:7,
-    counterAttackPower: 50,
-    resetAttackPower: function() {this.currentAttackPower = this.defaultAttackPower;},
-    resetHP: function() {this.currentHP = this.defaultHP;}
-};
-
-var Raichu = {
-    name:"Raichu", 
-    defaultHP: 100, 
-    currentHP: 100, 
-    defaultAttackPower:5,
-    currentAttackPower:5,
-    counterAttackPower: 50,
-    resetAttackPower: function() {this.currentAttackPower = this.defaultAttackPower;},
-    resetHP: function() {this.currentHP = this.defaultHP;}
-};
-
 var character = ""; //character chosen by user
 var defender = ""; //current defender
+var characterCurrentHP = 0;
+var characterCurrentAttachPower = 0;
+var defenderCurrentHP = 0;
 
 var outputData = function(){
     console.log("outputData")
-    for(var prop in character){
-        console.log(character[prop]);
-    }
-    for(var prop in defender){
-    console.log(defender[prop]);
-    }
+    console.log("current character:" + character);
+    console.log("current defender"  + defender);
 }
 
 var updateHTML = function(tag, value, append){
@@ -73,51 +26,57 @@ var updateHTML = function(tag, value, append){
 /***************/
 /*  Game Play  */
 /***************/
-//Setup/Reset game
+$(document).ready(function() {
 
-//User selects character, character to new section
-$(".pokemon").on("click", function() {
-    character = this;
+    //User selects character, character to new section
+    $(".pokemon").on("click", function() {
+        if (character) {
+            defender = this;
+            //character.addClass("defender");
+            $(defender).detach().appendTo("#defender-section");
+            defenderCurrentHP = defender.attr("defaultHP");
+        } else {
+            character = this;
+            //character.addClass("character");
+            $(character).detach().appendTo("#character-section");
+            characterCurrentHP = character.attr("defaultHP");
+        }
+        
+        outputData();
+        //set all as defenders that are not the chosen one from above
+        
+    });
 
-    //set all as defenders that are not the chosen one from above
+    //Each attack - When user clicks Attack button
+    var $attackBtn = $("#attack");
+    $attackBtn.on("click", function() {
+        //Decrease user hp and defender hp
+        characterCurrentHP -= defender.attr("counterAttackPower") ;
+        defenderCurrentHP -= characterCurrentAttachPower;
+
+        //User character attack power increases by it's base attack power
+        //defender attack power never changes
+        characterCurrentAttackPower += character.attr("defaultAttackPower");
+
+        //Log attacks to the screen
+        updateHTML("attack-log", "You attacked " + defender.name + "for " + characterCurrentAttackPower + "damage. <br>", true);
+        updateHTML("attack-log", defender.name + " + attacked you back for " + defender.attr("counterAttackPower") + "damage. <br>", true);
     
-});
-
-
-//User selects defender, move defender to new section
-$(".pokemon").on("click", function() {
-    defender = this;
-});
-
-//Each attack - When user clicks Attack button
-var $attackBtn = $("#attack");
-$attackBtn.on("click", function() {
-    //Decrease user hp and defender hp
-    character.currentHP -= defender.counterAttackPower;
-    defender.currentHP -= character.currentAttackPower;
+        //Check to see if won/lost
+        //If user HP = 0, you lost
+        if (characterCurrentHP = 0){
+            alert("You lost!");
+        }
+        //If all defender's HP = 0, you won
+        if (defender.defenderCurrentHP && isEmpty($("#start"))){
+            alert("You won!");
+        }
     
-    //User character attack power increases by it's base attack power
-    //defender attack power never changes
-    character.currentAttackPower += character.attackPower
+    });
 
-    //Log attacks to the screen
-    updateHTML("attack-log", "You attacked " + defender.name + "for " + character.currentAttackPowerattackPower + "damage.", true);
-    updateHTML("attack-log", defender.name + " + attacked you back for " + defender.counterAttackPower + "damage.", true);
-});
+    //Restart game
+    $("#restart").on("click", function() {
+        location.reload();
+    });
 
-//Check to see if won/lost
-//If user HP = 0, you lost
-
-//If all defender's HP = 0, you won
-
-//Restart game
-$("#restart").on("click", function() {
-    Blastoise.resetAttackPower;
-    Blastoise.resetHP;
-    Charizard.resetAttackPower;
-    Charizard.resetHP;
-    Venusar.resetAttackPower;
-    Venusar.resetHP;
-    Raichu.resetAttackPower;
-    Raichu.resetHP;
 });
